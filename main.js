3102934620e071ca44d8e24392b7168136749995
marton bognar <martonbognar@sch.bme.hu>
2016-08-17 05:11:42 +0200

    A bit of formatting and code cleanup

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ function loadRelease(repo, shortname) {
     success: function(data) {
       let assets = data.assets;
       let url = assets[0].browser_download_url;
-      let date = data.published_at;
+      let date = new Date(data.published_at);
       if (url != null) {
-        container.innerHTML = "<a href='" + url + "'>Download PDF (" + date + ")</a>";
+        container.innerHTML = "<a href='" + url + "'>Download PDF (" + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ")</a>";
       } else {
         container.innerHTML = "There was a problem with the release";
       }
     },
     error: function() {
-      container.innerHTML = "Could not load";
+      container.innerHTML = "No release exists yet";
     }
   });
 }
